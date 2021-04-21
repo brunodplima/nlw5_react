49e0803b0fd8a5c336a6c15a048ce7da916fa36e
Bruno Lima <brunodplima@gmail.com>
2021-04-21 00:39:51 -0300

    Criar layout do Player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
 import { Header } from '../components/Header';
+import { Player } from '../components/Player';
 
 import '../styles/globals.scss'
+import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-    </>
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
   )
 }
 
